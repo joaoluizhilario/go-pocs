b614d2abb9476e888e016f33d66c0fb87402a7f7
João Luiz Fernandes <joaoluiz@Laptop-de-Joao.local>
2024-02-23 22:58:47 -0300

    feat: Adicionando swaggo para Documentação de API

--- cmd/api/api.go
+++ cmd/api/api.go
@@ -2,13 +2,33 @@ package api
 
 import (
 	"github.com/gofiber/fiber/v2"
+	_ "github.com/joaoluizhilario/go-cockroach-poc/docs"
 	"github.com/joaoluizhilario/go-cockroach-poc/internal/database"
+	fiberSwagger "github.com/swaggo/fiber-swagger"
 )
+
+//	@title			Supermarket API
+//	@version		2.0
+//	@description	Supermarket API for studies.
+//	@termsOfService	http://swagger.io/terms/
+
+//	@contact.name	API Support
+//	@contact.url	http://www.swagger.io/support
+//	@contact.email	support@swagger.io
+
+//	@license.name	Apache 2.0
+//	@license.url	http://www.apache.org/licenses/LICENSE-2.0.html
+
+// @host		localhost:3000
+// @BasePath	/
+// @schemes	http
 
 func InitAPI() {
 	database.ConnectDb()
 
 	app := fiber.New()
+
+	app.Get("/swagger/*", fiberSwagger.WrapHandler)
 
 	setupRoutes(app)
 
--- internal/handlers/products.go
+++ internal/handlers/products.go
@@ -1,11 +1,21 @@
 package handlers
 
 import (
+	"fmt"
+
 	"github.com/gofiber/fiber/v2"
 	"github.com/joaoluizhilario/go-cockroach-poc/internal/database"
 	"github.com/joaoluizhilario/go-cockroach-poc/internal/models"
 )
 
+// GetProducts ... Get all products
+//
+//	@Summary		Get all products
+//	@Description	get all products
+//	@Tags			Products
+//	@Success		200	{array}		models.Product
+//	@Failure		404	{object}	object
+//	@Router			/product [get]
 func ListProducts(c *fiber.Ctx) error {
 	products := []models.Product{}
 	database.DB.Db.Find(&products)
@@ -13,6 +23,16 @@ func ListProducts(c *fiber.Ctx) error {
 	return c.Status(fiber.StatusOK).JSON(products)
 }
 
+// CreateProduct ... Create Product
+//
+//	@Summary		Create new product based on parameters
+//	@Description	Create new product
+//	@Tags			Products
+//	@Accept			json
+//	@Param			product	body		models.Product	true	"Product Data"
+//	@Success		200		{object}	object
+//	@Failure		400,500	{object}	object
+//	@Router			/product [post]
 func CreateProduct(c *fiber.Ctx) error {
 	product := new(models.Product)
 
@@ -27,6 +47,16 @@ func CreateProduct(c *fiber.Ctx) error {
 	return c.Status(fiber.StatusOK).JSON(product)
 }
 
+// CreateOrder ... Create Order
+//
+//	@Summary		Create new order based on parameters
+//	@Description	Create new order
+//	@Tags			Orders
+//	@Accept			json
+//	@Param			order	body		models.Order	true	"Order Data"
+//	@Success		200		{object}	object
+//	@Failure		400,500	{object}	object
+//	@Router			/order [post]
 func CreateOrder(c *fiber.Ctx) error {
 	orderBody := new(models.Order)
 
@@ -61,7 +91,8 @@ func CreateOrder(c *fiber.Ctx) error {
 	for _, item := range orderBody.Items {
 		var product models.Product
 
-		if err := tx.First(&product, "id = ?", item.ProductID).Error; err != nil {
+		if err := tx.Model(models.Product{ID: item.ProductID}).First(&product).Error; err != nil {
+			fmt.Println("Product not found")
 			tx.Rollback()
 			return err
 		}
@@ -70,7 +101,7 @@ func CreateOrder(c *fiber.Ctx) error {
 			OrderID:   order.ID,
 			ProductID: product.ID,
 			Quantity:  item.Quantity,
-			UnitPrice: item.UnitPrice,
+			UnitPrice: product.Price,
 		}
 
 		if err := tx.Create(&orderItem).Error; err != nil {
@@ -83,11 +114,17 @@ func CreateOrder(c *fiber.Ctx) error {
 
 	tx.Commit()
 
-	// database.DB.Db.Create(&orderBody)
-
 	return c.Status(fiber.StatusOK).JSON(orderBody)
 }
 
+// GetOrders ... Get all orders
+//
+//	@Summary		Get all orders
+//	@Description	get all orders hot reload
+//	@Tags			Orders
+//	@Success		200	{array}		models.Order
+//	@Failure		404	{object}	object
+//	@Router			/order [get]
 func ListOrders(c *fiber.Ctx) error {
 	orders := []models.Order{}
 	database.DB.Db.Preload("Items").Preload("Items.Product").Find(&orders)
